0108895e988655747d1ba29b244f2406603bf555
Cameron Reuschel <reuschel.cameron@live.de>
2025-02-23 23:26:12 +0100

    ComposedLens to save some closure allocations

--- DracTecOptics/LensExtensions.cs
+++ DracTecOptics/LensExtensions.cs
@@ -2,6 +2,14 @@
 
 public static class LensExtensions
 {
+    public static Func<TRecord, TValue> AsFunc<TRecord, TValue>(this IGetter<TRecord, TValue> getter) => getter.Get;
+    
+    public static TRecord Update<TRecord, TValue>(
+        this ILens<TRecord, TValue> lens, 
+        TRecord record, 
+        Func<TValue, TValue> update
+    ) => lens.Set(record, update(lens.Get(record)));
+    
     /// <summary>
     /// Composes two lenses into a new lens which accesses the nested property.
     /// Note that this might incur a performance penalty as compared to using the generated lenses directly.
@@ -9,10 +17,7 @@ public static class LensExtensions
     public static ILens<TRecord, TNewValue> Compose<TRecord, TValue, TNewValue>(
         this ILens<TRecord, TValue> self,
         ILens<TValue, TNewValue> next
-    ) => new BasicLens<TRecord, TNewValue>(
-        r => next.Get(self.Get(r)), 
-        (r, v) => self.Set(r, next.Set(self.Get(r), v))
-    );
+    ) => new ComposedLens<TRecord, TValue, TNewValue>(self, next);
     
     /// <summary>
     /// Composes a lens with additional getter and setter closures.
@@ -27,11 +32,13 @@ public static class LensExtensions
         (r, v) => self.Set(r, setter(self.Get(r), v))
     );
     
-    public static Func<TRecord, TValue> AsFunc<TRecord, TValue>(this IGetter<TRecord, TValue> getter) => getter.Get;
-    
-    public static TRecord Update<TRecord, TValue>(
-        this ILens<TRecord, TValue> lens, 
-        TRecord record, 
-        Func<TValue, TValue> update
-    ) => lens.Set(record, update(lens.Get(record)));
+    private sealed record ComposedLens<TRecord, TValue, TNewValue>(
+        ILens<TRecord, TValue> First, 
+        ILens<TValue, TNewValue> Second
+    ) : ILens<TRecord, TNewValue>
+    {
+        public TNewValue Get(TRecord theRecord) => Second.Get(First.Get(theRecord));
+        public TRecord Set(TRecord theRecord, TNewValue value) => 
+            First.Set(theRecord, Second.Set(First.Get(theRecord), value));
+    }
 }
